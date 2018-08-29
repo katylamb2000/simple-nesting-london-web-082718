6221b39f3cc30151fa87e8c6f6822bcd26814745
katy@karstaway.com <katy@karstaway.com>
2018-08-29 20:55:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -86,12 +86,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-   end
-# programmer_hash:yukihiro_matsumoto => {
-# :known_for => "Ruby",
-# :languages => ["LISP", "C"]}
+   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+     programmer_hash
 
-
+end
 
 def changing_alan
 	programmer_hash = 
